hebasto/bitcoin-qtw

Fix autostart filenames on Linux

src/qt/guiutil.cpp
@@ -639,7 +639,7 @@ fs::path static GetAutostartFilePath()
     std::string chain = gArgs.GetChainName();
     if (chain == CBaseChainParams::MAIN)
         return GetAutostartDir() / "bitcoin.desktop";
-    return GetAutostartDir() / strprintf("bitcoin-%s.lnk", chain);
+    return GetAutostartDir() / strprintf("bitcoin-%s.desktop", chain);
 }
 
 bool GetStartOnSystemStartup()

syscoin-core/syscoin

src/qt/guiutil.cpp
@@ -647,7 +647,7 @@ fs::path static GetAutostartFilePath()
     std::string chain = gArgs.GetChainName();
     if (chain == CBaseChainParams::MAIN)
         return GetAutostartDir() / "syscoin.desktop";
-    return GetAutostartDir() / strprintf("syscoin-%s.lnk", chain);
+    return GetAutostartDir() / strprintf("syscoin-%s.desktop", chain);
 }
 
 bool GetStartOnSystemStartup()