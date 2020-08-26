ryanofsky/bitcoin

gui: Replace interface::Node references with pointers
No change in behavior. Replacing references with pointers allows Node interface
creation to be delayed until later during gui startup next commit to support
implementing -ipcconnect option

src/qt/test/test_main.cpp
@@ -69,7 +69,8 @@ int main(int argc, char* argv[])
 
     // Don't remove this, it's needed to access
     // QApplication:: and QCoreApplication:: in the tests
-    BitcoinApplication app(*node);
+    BitcoinApplication app;
+    app.setNode(*node);
     app.setApplicationName("Bitcoin-Qt-test");
 
     node->context()->args = &gArgs;     // Make gArgs available in the NodeContext
@@ -82,7 +83,7 @@ int main(int argc, char* argv[])
     if (QTest::qExec(&test1) != 0) {
         fInvalid = true;
     }
-    RPCNestedTests test3(*node);
+    RPCNestedTests test3(app.node());
     if (QTest::qExec(&test3) != 0) {
         fInvalid = true;
     }
@@ -91,11 +92,11 @@ int main(int argc, char* argv[])
         fInvalid = true;
     }
 #ifdef ENABLE_WALLET
-    WalletTests test5(*node);
+    WalletTests test5(app.node());
     if (QTest::qExec(&test5) != 0) {
         fInvalid = true;
     }
-    AddressBookTests test6(*node);
+    AddressBookTests test6(app.node());
     if (QTest::qExec(&test6) != 0) {
         fInvalid = true;
     }

syscoin-core/syscoin

src/qt/test/test_main.cpp
@@ -68,10 +68,11 @@ int main(int argc, char* argv[])
 
     // Don't remove this, it's needed to access
     // QApplication:: and QCoreApplication:: in the tests
-    SyscoinApplication app(*node);
+    SyscoinApplication app;
+    app.setNode(*node);
     app.setApplicationName("Syscoin-Qt-test");
 
-    node->context()->args = &gArgs;     // Make gArgs available in the NodeContext
+    app.node().context()->args = &gArgs;     // Make gArgs available in the NodeContext
     AppTests app_tests(app);
     if (QTest::qExec(&app_tests) != 0) {
         fInvalid = true;
@@ -80,7 +81,7 @@ int main(int argc, char* argv[])
     if (QTest::qExec(&test1) != 0) {
         fInvalid = true;
     }
-    RPCNestedTests test3(*node);
+    RPCNestedTests test3(app.node());
     if (QTest::qExec(&test3) != 0) {
         fInvalid = true;
     }
@@ -89,11 +90,11 @@ int main(int argc, char* argv[])
         fInvalid = true;
     }
 #ifdef ENABLE_WALLET
-    WalletTests test5(*node);
+    WalletTests test5(app.node());
     if (QTest::qExec(&test5) != 0) {
         fInvalid = true;
     }
-    AddressBookTests test6(*node);
+    AddressBookTests test6(app.node());
     if (QTest::qExec(&test6) != 0) {
         fInvalid = true;
     }